24ba97f07ab20ffe27c50df39379eecb08b27440
Shimaa Magdi <shimaa.magdi1@gmail.com>
2018-02-12 23:08:14 +0200

    Edit localization

--- GeographicalApp/Utils/AppDelegate.swift
+++ GeographicalApp/Utils/AppDelegate.swift
@@ -11,7 +11,7 @@ import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     let soundName = "Default"
     let locationManager = CLLocationManager()
@@ -29,28 +29,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UIApplication.shared.cancelAllLocalNotifications()
         return true
     }
-
-func handleEvent(forRegion region: CLRegion!) {
-    // Show an alert if application is active
-    if UIApplication.shared.applicationState == .active {
-        guard let message = getNotificationMsg(fromRegionIdentifier: region.identifier) else { return }
-        window?.rootViewController?.showAlert(withTitle: nil, message: message)
-    } else {
-        // Otherwise present a local notification
-        let notification = UILocalNotification()
-        notification.alertBody = getNotificationMsg(fromRegionIdentifier: region.identifier)
-        notification.soundName = soundName
-        UIApplication.shared.presentLocalNotificationNow(notification)
+    
+    func handleEvent(forRegion region: CLRegion!) {
+        // Show an alert if application is active
+        if UIApplication.shared.applicationState == .active {
+            guard let message = getNotificationMsg(fromRegionIdentifier: region.identifier) else { return }
+            window?.rootViewController?.showAlert(withTitle: nil, message: message)
+        } else {
+            // Otherwise present a local notification
+            let notification = UILocalNotification()
+            notification.alertBody = getNotificationMsg(fromRegionIdentifier: region.identifier)
+            notification.soundName = soundName
+            UIApplication.shared.presentLocalNotificationNow(notification)
+        }
     }
-}
-
-func getNotificationMsg(fromRegionIdentifier name: String) -> String? {
-    let savedItems = UserDefaults.standard.array(forKey: UserDefaultsKeys.savedItems) as? [NSData]
-    let geotifications = savedItems?.map { NSKeyedUnarchiver.unarchiveObject(with: $0 as Data) as? GALocationViewModel }
-    let index = geotifications?.index { $0?.name == name }
-    return index != nil ? geotifications?[index!]?.name : nil
-}
-
+    
+    func getNotificationMsg(fromRegionIdentifier name: String) -> String? {
+        let savedItems = UserDefaults.standard.array(forKey: UserDefaultsKeys.savedItems) as? [NSData]
+        let geotifications = savedItems?.map { NSKeyedUnarchiver.unarchiveObject(with: $0 as Data) as? GALocationViewModel }
+        let index = geotifications?.index { $0?.name == name }
+        var msg = "WelcomeMsg".localized
+        if let msgIndex =  index {
+            msg = String(format: "WelcomeMsg".localized, (geotifications?[msgIndex]?.name)! )
+        }
+        return msg
+    }
+    
 }
 
 extension AppDelegate: CLLocationManagerDelegate {
